59425f845e698133f6988e673ace3aa9b06e4097
Lucas <lucasbegue2@gmail.com>
2021-10-30 18:56:20 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,113 @@
+import os
+import uuid
+import flask
+import urllib
+import requests
+from PIL import Image
+from PIL import Image
+from io import BytesIO
+from tensorflow.keras.models import load_model
+from flask import Flask , render_template  , request , send_from_directory
+from tensorflow.keras.preprocessing.image import load_img , img_to_array
+import cv2
+from classifier import *
+
+app = Flask(__name__)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+ALLOWED_EXT = set(['jpg', 'jpeg', 'png', 'jfif'])
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXT
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/success', methods=['GET' , 'POST'])
+def success():
+    error = ''
+    target_img = os.path.join(os.getcwd(), 'uploads')
+
+    if request.method == 'POST':
+
+        if request.files:
+            file = request.files['file']
+
+            if file and allowed_file(file.filename):
+
+                img_path = os.path.join(target_img, file.filename)
+                file.save(img_path)
+                img = cv2.imread(img_path)
+
+                if request.form['submit_button'] == "Blur all faces":
+
+                    face_boxes = detect_all_faces(img_path)
+                    blurred_img = blur_faces(img, face_boxes)
+                    img_filename = 'blurred.jpg'
+                    cv2.imwrite(os.path.join('static/blurred',  img_filename), blurred_img)
+
+                elif request.form['submit_button'] == "Blur child faces":
+
+                    face_boxes = detect_child_faces(img_path)
+                    blurred_img = blur_faces(img, face_boxes)
+                    img_filename = 'blurred.jpg'
+                    cv2.imwrite(os.path.join('static/blurred', img_filename), blurred_img)
+
+            else:
+                error = "Please upload images of .jpg , .jpeg and .png extension only."
+
+            if len(error) == 0:
+                return render_template('success.html', img=img_filename)
+            else:
+                return render_template('index.html', error=error)
+
+    if request.form:
+        url = request.form.get('link')
+        try:
+            response = requests.get(url)
+            img = Image.open(BytesIO(response.content))
+            img_path = os.path.join(target_img, 'img_from_link.jpg')
+            img.save(img_path)
+            img = cv2.imread(img_path)
+
+            if request.form['submit_button'] == "Blur all faces":
+
+                face_boxes = detect_all_faces(img_path)
+                blurred_img = blur_faces(img, face_boxes)
+                img_filename = 'blurred.jpg'
+                cv2.imwrite(os.path.join('static/blurred', img_filename), blurred_img)
+
+            elif request.form['submit_button'] == "Blur child faces":
+
+                face_boxes = detect_child_faces(img_path)
+                blurred_img = blur_faces(img, face_boxes)
+                img_filename = 'blurred.jpg'
+                cv2.imwrite(os.path.join('static/blurred', img_filename), blurred_img)
+
+        except Exception as e:
+            print(str(e))
+            error = 'This image from this site is not accesible or inappropriate input.'
+
+        if len(error) == 0:
+            return render_template('success.html', img=img_filename)
+        else:
+            return render_template('index.html', error=error)
+
+    else:
+        return render_template('index.html')
+
+
+@app.route('/uploads/<path:filename>', methods=['GET', 'POST'])
+def download(filename):
+    return send_from_directory(directory=os.getcwd()+"/static/blurred", filename="filename.txt")
+
+if __name__ == "__main__":
+    app.run(debug = True)
+
+
---
+++ classifier.py
@@ -0,0 +1,70 @@
+import numpy as np
+import cv2
+from mtcnn.mtcnn import MTCNN
+from tensorflow.keras.models import load_model
+
+
+detector = MTCNN()
+model = load_model('training/models/final_model.h5')
+
+
+def rescale(img):
+    return np.add(np.multiply(img, 2/255), -1)
+
+
+def detect_all_faces(img_path):
+    img = cv2.imread(img_path)
+    detections = detector.detect_faces(img)
+    boxes = []
+
+    for det in detections:
+        boxes.append(det['box'])
+
+    return np.array(boxes)
+
+
+def detect_child_faces(img_path):
+    img = cv2.imread(img_path)
+    detections = detector.detect_faces(img)
+    faces = []
+    boxes = []
+    original_shapes = []
+
+    for det in detections:
+        boxes.append(det['box'])
+        (x, y, w, h) = det['box']
+        cropped_face = img[y:y+h, x:x+w]
+        cropped_face = cv2.resize(cropped_face, (128, 128))
+        cropped_face = rescale(cropped_face)
+        faces.append(cropped_face)
+
+    faces = np.array(faces)
+    predictions = np.squeeze(model.predict(faces), axis=-1)
+    child_indexes = [1 if pred>0.5 else 0 for pred in predictions]
+    child_faces = []
+
+    for i in range(len(boxes)):
+        if child_indexes[i] == 1:
+            child_faces.append(boxes[i])
+
+    return np.array(child_faces)
+
+
+def blur_faces(img, boxes):
+    for box in boxes:
+        (x, y, w, h) = box
+        face = img[y:y+h, x:x+w]
+        face_blurred = blur(face)
+        img[y:y+h, x:x+w] = face_blurred
+    return img
+
+
+def blur(img, interpolation=cv2.INTER_NEAREST):
+    h, w, _ = img.shape
+    ratio = h/w
+    h_ = 8
+    w_ = int(h_/ratio)
+    img_ = cv2.resize(img, dsize=(w_, h_), interpolation=interpolation)
+    img_blurred = cv2.resize(img_, dsize=(w, h), interpolation=cv2.INTER_NEAREST)
+    return img_blurred
+
